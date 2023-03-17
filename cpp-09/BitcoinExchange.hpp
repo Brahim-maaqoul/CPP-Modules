216adb3e7af07153c8417fd1db5ecde69fbc56c8
Brahim  Maaqoul <wacakoule@gmail.com>
2023-03-17 14:46:30 +0100

    starting cpp-09

---
+++ cpp-09/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmaaqoul <bmaaqoul@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/17 14:03:14 by bmaaqoul          #+#    #+#             */
+/*   Updated: 2023/03/17 14:28:20 by bmaaqoul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <map>
+
+class BitcoinExchange
+{
+	private:
+		/* data */
+	public:
+		BitcoinExchange(/* args */);
+		~BitcoinExchange();
+};
+
