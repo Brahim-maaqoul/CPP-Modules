0e0bac445aa152aad0c02482bee87b8fa8f3af1f
Brahim  Maaqoul <wacakoule@gmail.com>
2023-03-25 04:56:07 +0000

    .

--- cpp-09/ex01/main.cpp
+++ cpp-09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bmaaqoul <bmaaqoul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 22:10:11 by bmaaqoul          #+#    #+#             */
-/*   Updated: 2023/03/25 01:14:59 by bmaaqoul         ###   ########.fr       */
+/*   Updated: 2023/03/25 03:19:09 by bmaaqoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int ac, char **av)
 {
 	if (ac != 2)
 	{
-		std::cout << "Error: the program rerquire only one argument.\n";
+		std::cout << "Error: the program rerquire one argument.\n";
 		return 0;
 	}
 	rpn(av[1]);
